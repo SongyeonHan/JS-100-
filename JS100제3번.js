22611339b7a262e01257881be368b579adfb3c73
SongyeonHan <93498183+SongyeonHan@users.noreply.github.com>
2021-12-01 17:44:57 +0900

    3번 문제
    3번 문제 풀이

---
+++ JS100제3번.js
@@ -0,0 +1,12 @@
+// 3
+Object
+// JS에서는 배열 Array의 타입을 별도로 제공하지 않고 Object를 처리한다. 이 변수가 배열 Array인지 확인하기 위해서는 이런 방법들을 쓸 수 있다. 
+Array.isArray(arr);
+
+// 오늘의 정리! 배열 수정 방법 (추가, 삭제)
+
+// 배열 추가 : Array.push(), Array.unshift(), Array.splice()
+//            뒤에 추가      앞에 추가         지정한 위치에 추가
+
+// 배열 삭제 : Array.pop(), Array.shift(), Array.splice()
+//  
