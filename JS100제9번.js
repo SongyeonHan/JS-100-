d1c689098362cfc20fb1b477c763204ae2320e76
SongyeonHan <93498183+SongyeonHan@users.noreply.github.com>
2021-12-03 18:00:04 +0900

    9번 문제
    9번 문제 풀이

---
+++ JS100제9번.js
@@ -0,0 +1,11 @@
+var year = '2019';
+var month = '04';
+var day = '26';
+var hour = '11';
+var minute = '34';
+var second = '27';
+
+var result = year.concat('/', month, '/', day, ' ', hour, ':', minute, ':', second);
+console.log(result);
+
+// concat 은 배열과 문자열을 합쳐주는 메서드다
