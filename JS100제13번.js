f6e6fa92ab7ed3b4bc09d217b78b6a4f20356a41
SongyeonHan <93498183+SongyeonHan@users.noreply.github.com>
2021-12-20 13:07:44 +0900

    13번 문제
    13번 문제 풀이

--- JS100제13번.js
+++ JS100제13번.js
@@ -1,4 +1,4 @@
-let planet = ['0번째 행성은 없습니다!', 수성, 금성, 지구, 화성, 목성, 토성, 천왕성, 해왕성]
+let planet = ['0번째 행성은 없습니다!', '수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성']
 
 let input = prompt();
-document.write();
+alert(planet[input]);
